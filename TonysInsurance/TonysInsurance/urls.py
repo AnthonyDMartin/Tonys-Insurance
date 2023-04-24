93e4003737167e3fb99dfcad557eb4990a4ea78e
Tony <127994243+AnthonyDMartin@users.noreply.github.com>
2023-04-24 13:51:24 +0200

    project in Czech

--- TonysInsurance/TonysInsurance/urls.py
+++ TonysInsurance/TonysInsurance/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib import admin
-from django.urls import path
 from pojisteni import views
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, include
